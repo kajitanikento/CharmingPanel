92dc51f56f7dcf1290a719c1eb9b17dfab16fd91
kajitanikento <kajitanikento03@gmail.com>
2025-11-29 12:15:15 +0900

    Initial Commit

---
+++ InputSourceDisplayApp/InputSourceDisplayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InputSourceDisplayAppApp.swift
+//  InputSourceDisplayApp
+//
+//  Created by kajitani kento on 2025/11/29.
+//
+
+import SwiftUI
+
+@main
+struct InputSourceDisplayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
